a0ce6d471b9281747e9b83fe9ebae88077a719fb
ruinny <admin@imgki.com>
2023-03-14 19:38:10 +0800

    update 20230314

--- get_daily_news.py
+++ get_daily_news.py
@@ -1,19 +1,19 @@
 from get_news_model import *
 from send_daily_model import *
 
-get_date = datetime.now() - timedelta(days=2)
+get_date = datetime.now() - timedelta(days=1)
 # 加载获取新闻的日期
 print("获取新闻的日期已设定为" + get_date.strftime('%Y-%m-%d'))
 
+
+news = News('三茅人力资源', get_date)
+num = news.insert_db()
+del news
 
 news = News('组织人事报',get_date)
 date_today = get_date.strftime('%Y-%m-%d')
 url = f'http://www.zuzhirenshi.com/dianzibao/{date_today}/1/index.htm'
 page_url = news.get_news_pages(url)
-num = news.insert_db()
-del news
-
-news = News('三茅人力资源', get_date)
 num = news.insert_db()
 del news
 
